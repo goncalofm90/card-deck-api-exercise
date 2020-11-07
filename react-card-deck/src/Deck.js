d37819e0e74026cc5618c896133ffd9c1e0e8bb7
goncalofm90 <goncalofm90@gmail.com>
2020-11-07 14:25:55 +0000

    finished exercise

---
+++ react-card-deck/src/Deck.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Card from './Card';
+import './Deck.css';
+import axios from 'axios';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck/';
+
+
+export class Deck extends Component {
+  constructor(props){
+    super(props);
+    this.state = {deck: null, drawn:[]};
+    this.getCard = this.getCard.bind(this);
+  }
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+    this.setState({deck: deck.data});
+  }
+
+  async getCard(){
+    let deck_id = this.state.deck.deck_id;
+    try{
+      let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+      //make request using deck id
+      let cardRes = await axios.get(cardUrl);
+      if(!cardRes.data.success){
+        throw new Error('No cards remaining')
+      }
+      console.log(cardRes.data);
+      let card = cardRes.data.cards[0];
+      //set state using new card info from api
+      this.setState(state => ({
+        drawn: [
+        ...state.drawn,
+        {
+          id: card.code, 
+          image: card.image, 
+          name: `${card.value} of ${card.suit}`
+         }
+        ]
+      }));
+    } catch(err) {
+      alert(err);
+    }
+  
+  }
+
+  render() {
+    const cards = this.state.drawn.map(card => (
+      <Card 
+      key={card.id} 
+      name={card.name} 
+      image = {card.image} 
+      />
+    ));
+    return (
+      <div className='Deck'>
+        <h1 className='Deck-title'>⬥ Card Dealer ⬥</h1>
+        <h2 className='Deck-title subtitle'>React Card Drawing Exercise</h2>
+        <button className='Deck-btn' onClick={this.getCard}>Draw Card</button>
+        <div className='Deck-cardarea'>
+        {cards}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Deck
